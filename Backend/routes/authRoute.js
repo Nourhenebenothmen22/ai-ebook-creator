2145bf9475712220cdae566881c1f63b0f42e1f6
nourhene <benothmennourhen8@gmail.com>
2025-10-19 20:06:04 +0100

    🛤️ Routes Auth : ajout des endpoints getProfile et updateProfile pour gestion du profil utilisateur

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login,getProfile,updateProfile } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/profile',getProfile)
+router.put('/:id',updateProfile)
 
 module.exports = router;
