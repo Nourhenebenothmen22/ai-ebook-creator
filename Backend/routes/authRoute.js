4d3efaf8d9e16b1dd0328af1aef98c88165c4b01
nourhene <benothmennourhen8@gmail.com>
2025-10-19 20:13:33 +0100

    🛡️ Routes Auth : ajout du middleware protect pour sécuriser getProfile et updateProfile

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { register, login,getProfile,updateProfile } = require('../controllers/authController');
+const protect=require('../middlewares/authMiddleware')
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/profile',getProfile)
-router.put('/:id',updateProfile)
+router.get('/profile',protect,getProfile)
+router.put('/:id',protect,updateProfile)
 
 module.exports = router;
